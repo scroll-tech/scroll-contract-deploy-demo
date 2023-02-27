b782350758223e48baf8d5bc36eb82414783e5ec
Filip Pajic <42151917+pajicf@users.noreply.github.com>
2023-02-27 16:49:24 +0100

    Updated urls for the alpha (#3)

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -13,7 +13,8 @@ async function main() {
   await lock.deployed();
 
   console.log(`Lock with 0.00000001 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-  console.log(`Block explorer URL: https://l2scan.scroll.io/address/${lock.address}`);
+  // console.log(`Block explorer URL: https://l2scan.scroll.io/address/${lock.address}`); Uncomment here to use the pre-alpha
+  console.log(`Block explorer URL: https://blockscout.scroll.io/address/${lock.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
